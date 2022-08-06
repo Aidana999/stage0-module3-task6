df7441cbb4fa034491a020b2ca8656bd8a3882dd
Aidana <49874037+Aidana999@users.noreply.github.com>
2022-08-06 20:45:39 +0600

    Update PrePostIncrement.java

--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -2,9 +2,9 @@ package lang.print.gaps.task6;
 
 public class PrePostIncrement {
     public static void main(String[] args) {
-        int a = 5;
+        int a = ++5;
         int b = 7;
-        int c = 12;
+        int c = 12++;
         int d = 15;
 
         // use post-increment operator in the mathematical expression  
